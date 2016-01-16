7acf319ac16f38fd4c0ab6031843739f3b4c33ca
Bob Webb <bob.wbb@googlemail.com>
2016-01-16 22:56:04 +0000

    in the edit ticket page, the email address is shown in the 'Assignees' input

--- tracker/site/forms.py
+++ tracker/site/forms.py
@@ -40,9 +40,12 @@ class ProjectForm(BaseTrackerForm):
     def pre_save(self, instance):
         instance.created_by = self.user
 
+class EmailChoiceField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj.email
 
 class TicketForm(BaseTrackerForm):
-    assignees = forms.ModelMultipleChoiceField(queryset=None, required=False)
+    assignees = EmailChoiceField(queryset=None, required=False)
 
     class Meta:
         model = Ticket
